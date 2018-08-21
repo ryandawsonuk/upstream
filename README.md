50051d714eab63b74583ea1f6a3e8be563f1ac71
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-21 14:34:25 +0100

    initial commit for testing updatebot

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic example of upstream library repo to test updatebot
---
+++ src/main/java/org/example/upstream/UpstreamProperties.java
@@ -0,0 +1,39 @@
+package org.example.upstream;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:upstream.properties")
+@ConfigurationProperties(prefix = "upstream")
+public class UpstreamProperties {
+
+    public String groupId;
+    public String name;
+    public String version;
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
